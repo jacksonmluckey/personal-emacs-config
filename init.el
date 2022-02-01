03b5a51d0ec551b9d5a9f5e7d70fd3b5a0f42543
Jackson M Luckey <7769009+jacksonmluckey@users.noreply.github.com>
2022-02-01 17:58:48 -0300

    added very stripped down version of init.el from work pc

---
+++ init.el
@@ -0,0 +1,35 @@
+;; Require MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Install if not installed and then enable use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure 't)
+
+;; Install if not installed and then use Monokai theme
+(unless (package-installed-p 'monokai-theme)
+  (package-refresh-contents)
+  (package-install 'monokai-theme))
+(load-theme 'monokai t)
+
+;; Use EVIL
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+(require 'evil)
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil monokai-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
